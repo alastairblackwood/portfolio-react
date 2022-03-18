5c7d35cb7d851a7cad967fb0c32877242dcaa50d
alastairblackwood <agrizzlybear@icloud.com>
2022-03-18 15:16:00 +1000

    added slide transition css on homepage text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,36 @@
 import * as React from "react";
-import { Box, Text, Link, VStack, Heading, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Link,
+  VStack,
+  Heading,
+  Flex,
+  keyframes,
+} from "@chakra-ui/react";
 import { Logo } from "../../containers/Logo/Logo";
 
-export const Home = () => (
-  <Flex justifyContent="center" maxW="1200px" margin="0 auto" w="100%" p={3}>
-    <VStack spacing={2}>
-      <Logo pointerEvents="none" mt={20} p={6} />
+const moveInLeft = keyframes`0% { opacity: 0; transform: translateX(-100px); }
+100% { opacity: 1; transform: translateX(0); } }`;
 
-      <Heading>Alastair Blackwood</Heading>
-      <Text as="h6">Junior FullStack Developer</Text>
-    </VStack>
-  </Flex>
-);
+const moveInRight = keyframes`0% { opacity: 0; transform: translateX(100px); }
+100% { opacity: 1; transform: translateX(0); } }`;
+
+export const Home = () => {
+  const slideLeftAnimation = `${moveInLeft} 3s ease-out`;
+  const slideRightAnimation = `${moveInRight} 3s ease-out`;
+
+  return (
+    <Flex justifyContent="center" maxW="1200px" margin="0 auto" w="100%" p={3}>
+      <VStack spacing={2}>
+        <Logo pointerEvents="none" mt={20} p={6} mb={4} />
+        <Box animation={slideLeftAnimation}>
+          <Heading>Alastair Blackwood</Heading>
+        </Box>
+        <Box animation={slideRightAnimation}>
+          <Text as="h6">Junior FullStack Developer</Text>
+        </Box>
+      </VStack>
+    </Flex>
+  );
+};
